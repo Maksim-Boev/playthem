f9fff2ec6bf4ce86bc32838c96a905d59e4437e0
Maksim <maksim.schetina@gmail.com>
2021-07-02 12:58:34 +0300

    changed path flag icon

--- src/shared/constants/flags.js
+++ src/shared/constants/flags.js
@@ -1,15 +1,15 @@
-import { ReactComponent as Ru } from '../assets/icons/flags/RU.svg';
-import { ReactComponent as Fr } from '../assets/icons/flags/FR.svg';
-import { ReactComponent as En } from '../assets/icons/flags/US.svg';
-import { ReactComponent as De } from '../assets/icons/flags/DE.svg';
-import { ReactComponent as He } from '../assets/icons/flags/IL.svg';
+import fr from '../assets/icons/flags/fr.svg';
+import ru from '../assets/icons/flags/ru.svg';
+import en from '../assets/icons/flags/us.svg';
+import de from '../assets/icons/flags/de.svg';
+import he from '../assets/icons/flags/il.svg';
 
 const flags = {
-  ru: <Ru />,
-  fr: <Fr />,
-  en: <En />,
-  de: <De />,
-  he: <He />
+  ru,
+  fr,
+  en,
+  de,
+  he
 };
 
 export default flags;
